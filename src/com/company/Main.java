41843e9559a7a3a83478e05ae6a62b311c46a515
CelineTrann <celinephoungtran@gmail.com>
2020-08-20 20:40:06 -0400

    Move viewing stats to separate function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,38 @@ import java.util.Scanner;
 
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+
+        System.out.println("Hello! Welcome to your new adventure. What is your name?");
+        String name = user.nextLine();
+
+        System.out.println("Welcome " + name + "! What race are your?");
+        String race = user.nextLine();
+
+        Player player1 = new Player(name, race, 1);
+
+        viewStats(user, player1);
+
+        if(player1.weapon.currentWeapon.equalsIgnoreCase("none")) {
+            System.out.println("Oh! It looks like you have no weapons.");
+        } else {
+            System.out.println("Current weapon" + player1.weapon.currentWeapon);
+        }
+
+        //buy weapon
+        buyWeapon(user, player1);
+
+        //upgrade weapon
+        upgradeWeapon(user, player1);
+
+        System.out.println("Add gold and buy (y/n)");
+        if(user.nextLine().equalsIgnoreCase("y")){
+            player1.setGold(false, 5);
+
+        }
+
+    }
 
     public static void buyWeapon(Scanner user, Player player1){
         System.out.println("Opening shop... (Hit ENTER to continue)");
@@ -26,42 +58,10 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
-        Scanner user = new Scanner(System.in);
-
-        System.out.println("Hello! Welcome to your new adventure. What is your name?");
-        String name = user.nextLine();
-
-        System.out.println("Welcome " + name + "! What race are your?");
-        String race = user.nextLine();
-
-        System.out.println("Well that's great! Now, what class do you want to play?");
-        user.nextLine();
-
-        System.out.println("Whoops. There seems to be a problem. The only class available is 'Wizard' :(");
-        Player player1 = new Player(name, race, 1);
-
+    public static void viewStats(Scanner user, Player player1) {
         System.out.println("Would you like to see your overall stats? (y/n)");
         if(user.nextLine().equalsIgnoreCase("y")){
             player1.printPlayerInfo();
         }
-
-        if(player1.weapon.currentWeapon.equalsIgnoreCase("none")) {
-            System.out.println("Oh! It looks like you have no weapons.");
-        } else {
-            System.out.println("Current weapon" + player1.weapon.currentWeapon);
-        }
-
-        //buy weapon
-        buyWeapon(user, player1);
-
-        upgradeWeapon(user, player1);
-
-        System.out.println("Add gold and buy (y/n)");
-        if(user.nextLine().equalsIgnoreCase("y")){
-            player1.setGold(false, 5);
-
-        }
-
     }
 }
