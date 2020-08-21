b28c4e5ea0696ded0da176a6fdcaf6fe483f5672
CelineTrann <celinephoungtran@gmail.com>
2020-08-20 20:33:16 -0400

    Moved buyWeapon in main to own function for organization; can be reused

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void buyWeapon(Scanner user, Player player1){
+        System.out.println("Opening shop... (Hit ENTER to continue)");
+        user.nextLine();
+        player1.weapon.showWeaponInfo();
+
+        System.out.println("What would you like to buy? (Case sensitive)");
+        System.out.println("Current gold: " + player1.getGold());
+        String purchase = user.nextLine();
+        player1.buyWeapon(purchase);
+    }
+
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in);
 
@@ -30,18 +41,19 @@ public class Main {
         } else {
             System.out.println("Current weapon" + player1.weapon.currentWeapon);
         }
-        System.out.println("Opening shop... (Hit ENTER to continue)");
-        user.nextLine();
-        player1.weapon.showWeaponInfo();
 
-        System.out.println("What would you like to buy? (Case sensitive)");
-        System.out.println("Current gold: " + player1.getGold());
-        String purchase = user.nextLine();
-        player1.buyWeapon(purchase);
+        //buy weapon
+        buyWeapon(user, player1);
 
         System.out.println("Would you like to upgrade your weapon? (y/n)");
         if(user.nextLine().equalsIgnoreCase("y")){
             player1.weapon.showUpgradeWeapon();
+        }
+
+        System.out.println("Add gold and buy (y/n)");
+        if(user.nextLine().equalsIgnoreCase("y")){
+            player1.setGold(false, 5);
+
         }
 
     }
