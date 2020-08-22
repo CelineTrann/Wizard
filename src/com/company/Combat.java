69eeb385cdcb561cb8d3c95d969ed412195db400
CelineTrann <celinephoungtran@gmail.com>
2020-08-21 23:05:45 -0400

    Clean up comments

--- src/com/company/Combat.java
+++ src/com/company/Combat.java
@@ -4,38 +4,17 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Combat extends BaseMech {
+    //------------------- ATTRIBUTES --------------------
     String[] creatures = {"none", "giantWeasel", "giantFrog", "hobgoblin", "brownBear"};
     Enemy enemy;
     boolean exit;
 
+    //------------------- CONSTRUCTORS --------------------
     public Combat(Player player1){
         int enemyIndex = getEnemyIndex(player1.getLevel());
         enemy = new Enemy(creatures[enemyIndex]);
     }
 
-    //return true when exit or enemy died
-//    public boolean encounter(Scanner user, Player player1){
-//        int enemyIndex = getEnemyIndex(player1.getLevel());
-//        enemy = new Enemy(creatures[enemyIndex]);
-//
-//        while(!died(player1) && !exit){
-//            System.out.println("A " + enemy.getName()+ " appeared!");
-//            System.out.println("What will you do?");
-//
-//            while(playerOption(user, player1)){
-//                System.out.print("\t1. Attack \n" +
-//                        "\t2. Defend \n" +
-//                        "\t3. Run");
-//            }
-//
-//            if(!died(player1) && !exit){
-//                attackPlayer(player1);
-//            }
-//            player1.setAC(true);
-//        }
-//        //enemy is dead or user wants to run
-//        return true;
-//    }
 
     private int getEnemyIndex(int level){
         Random randomEnemy = new Random();
@@ -43,25 +22,6 @@ public class Combat extends BaseMech {
     }
 
     //------------------- PLAYER ACTION --------------------
-    //TODO delete this?
-//    public boolean playerOption(Scanner user, Player player1){
-//        switch (user.nextLine()){
-//            case "attack":
-//                attack(player1);
-//                break;
-//            case "defend":
-//                defend(player1);
-//                break;
-//            case "run":
-//                exit = run(player1);
-//                break;
-//            default:
-//                System.out.println("Invalid option");
-//                return true;
-//        }
-//        return false;
-//    }
-
     //return true if enemy is hit
     public int attack(Player player1){
         if(player1.weapon.attackRoll(enemy)) {
